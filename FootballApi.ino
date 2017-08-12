3ff755dcd4aa0e140c825c9ef581d89918889ea9
Yash Soni <yashsoni2017@gmail.com>
2017-08-13 00:42:15 +0530

    Update FootballApi.ino

--- FootballApi.ino
+++ FootballApi.ino
@@ -1,30 +1,41 @@
 
+
 void Live_score()
 {
     HTTPClient http;    //Declare object of class HTTPClient
     DynamicJsonBuffer jsonBuffer;   
     http.begin("http://api.football-api.com/2.0/matches/2212909?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76");
-     delay(200);
+     delay(5000);
      int httpCode = http.GET(); //Send the request
      String payload = http.getString();   
      //Serial.println(httpCode);   //Print HTTP return code 200 correct data 401 invalid data  404 Error
      Serial.println(payload);    //Print request response payload
      JsonObject& root = jsonBuffer.parseObject(payload);
-   
+    
        if(!root.success()){
-             Serial.println("Nope");
+             Serial.println("Nope");          
               }
-           else{
-        
-        String LocalTeamScore = root["localteam_score"];
-        String AwayTeamScore = root["visitorteam_score"];
-        String Display = LocalTeamScore + " - " + AwayTeamScore;
-        display.setTextAlignment(TEXT_ALIGN_LEFT);
-        display.setFont(ArialMT_Plain_24);
-        display.drawString(50,15,Display);
-        display.display();
-        
-       }
+           else{       
+                String LocalTeamScore = root["localteam_score"];
+                String AwayTeamScore = root["visitorteam_score"];
+                String LocalTeamName = root["localteam_name"];
+                String AwayTeamName = root["visitorteam_name"];
+                String Display = LocalTeamScore + " - " + AwayTeamScore;
+                String Names = LocalTeamName + " - " + AwayTeamName;
+                String Status = root["status"];
+                display.setTextAlignment(TEXT_ALIGN_LEFT);
+                display.setFont(ArialMT_Plain_10);
+                display.drawString(20,0,Names);
+                display.setTextAlignment(TEXT_ALIGN_LEFT);
+                display.setFont(ArialMT_Plain_24);
+                display.drawString(40,20,Display);
+                display.setTextAlignment(TEXT_ALIGN_LEFT);
+                display.setFont(ArialMT_Plain_24);
+                display.drawString(55,50,Status);
+                display.display();  
+                   
+                }
   
 }
+
 
