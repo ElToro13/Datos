d3a0f997ff6b53f486b4f793bb2976e398ffff68
Yash Soni <yashsoni2017@gmail.com>
2017-09-03 17:36:57 +0530

    Update MyProject.ino

--- MyProject.ino
+++ MyProject.ino
@@ -1,18 +1,26 @@
+#include <stdio.h>
 #include "setup.h"
 #include "City_ID.h"
 #include "Display_Selector.h"
 #include "FootballApi.h"
 #include "Reminder.h"
 #include "trainNo_Selector.h"
-
+#include "ChangeConnection.h"
+#include "clock.h"
 //unsigned int i=0,j=0,check,k=1;
 
-void setup() {  
+int sel;
+void (* fun_sel[6])() = {ChangeWifi, getCityID, Live_score, reminder, WATCH_display, trainNo_Selector};
+void setup() { 
   System_Init();
 }
+
 void loop() {
-   display.clear();
-    server.handleClient();
-    Live_score();
+display.clear();
+  server.handleClient();
+  for(sel=0;sel<6;sel++){
+   fun_sel[sel]();
+   delay(1000);
+  }
 }
 
